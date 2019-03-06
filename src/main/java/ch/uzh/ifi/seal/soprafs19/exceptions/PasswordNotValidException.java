980ef40b607439d33a21fa5f43949fbd842a2057
Julian Minder <>
2019-03-06 14:32:33 +0100

    Updated httpstatus for PasswordNotValidException to 401

--- src/main/java/ch/uzh/ifi/seal/soprafs19/exceptions/PasswordNotValidException.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/exceptions/PasswordNotValidException.java
@@ -3,7 +3,7 @@ package ch.uzh.ifi.seal.soprafs19.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
 public class PasswordNotValidException extends RuntimeException {
     public PasswordNotValidException(String name) {
         super("Password for user " + name + " is not valid!");
